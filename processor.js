b0c2a3cffbf4eb488c7328d6deb26e34e70e57e3
berlina.li <berlina.li@akqa.com>
2019-06-15 11:44:19 +0800

    testing fail

--- processor.js
+++ processor.js
@@ -13,19 +13,30 @@ let processor = {
         }, 0);
     },
 
+    timerCallbackDelayed: function() {
+      if (this.video.paused || this.video.ended) {
+        return;
+      }
+      this.computeFrameDelayed();
+
+      let self = this;
+      setTimeout(function () {
+        self.timerCallbackDelayed();
+      }, 1000);
+    },
+
     doLoad: function() {
       this.video = document.getElementById("video");
       this.c1 = document.getElementById("c1");
       this.ctx1 = this.c1.getContext("2d");
       this.c2 = document.getElementById("c2");
       this.ctx2 = this.c2.getContext("2d");
+      this.c3 = document.getElementById("c3");
+      this.ctx3 = this.c3.getContext("2d");
       let self = this;
 
       navigator.mediaDevices.getUserMedia({video: true, audio: false})
       .then(function(stream) {
-        // video.srcObject = stream;
-        // photo.setAttribute('src', data);
-        // this.video.setAttribute('src', stream);
         this.video.srcObject = stream;
         this.video.play();
       })
@@ -33,10 +44,13 @@ let processor = {
         console.log("An error occurred: " + err);
       });
 
-      this.video.addEventListener("play", function() {
+      this.video.addEventListener("canplay", function() {
           self.width = self.video.videoWidth;
           self.height = self.video.videoHeight;
           self.timerCallback();
+          setInterval(function(){
+            self.timerCallbackDelayed();
+          }, 1500);
         }, false);
     },
 
@@ -56,6 +70,28 @@ let processor = {
         }
       }
       this.ctx2.putImageData(frame, 0, 0);
+      return;
+    },
+
+    computeFrameDelayed: function() {
+      this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+      let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+      let l = frame.data.length / 4;
+
+      for (let i = 0; i < l; i++) {
+        let r = frame.data[i * 4 + 0];
+        let g = frame.data[i * 4 + 1];
+        let b = frame.data[i * 4 + 2];
+        // 89 87 89
+        // if (g > 100 && r > 100 && b < 43)
+        if (g < 100 && r < 100 && b < 100) {
+          frame.data[i * 4 + 3] = 0;
+        }
+      }
+      // setTimeout(function () {
+      //   this.ctx3.putImageData(frame, 0, 0);
+      // }, 3000);
+      this.ctx3.putImageData(frame, 0, 0);
       return;
     }
   };
